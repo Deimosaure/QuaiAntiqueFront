ec1ca02ac55b7911f6d0c3550eccb00dab1cd12b
Deimosaure <162324172+Deimosaure@users.noreply.github.com>
2024-05-31 15:26:54 +0200

    update

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,9 +5,11 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html", "/js/galerie.js"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", "/js/auth/signup.js"),
     new Route("/account", "Mon Compte", "/pages/auth/account.html"),
-    new Route("/editPassword", "Chagement de Mot de passe", "/pages/auth/editPassword.html")
+    new Route("/editPassword", "Chagement de Mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa","Vos réservations", "/pages/reservations/allResa.html"),
+    new Route("/reserver", "Reserver", "/pages/reservations/reserver.html" )
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
